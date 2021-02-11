f42a23de0ec7e4f9cc0fbb06a0b3d690e1814854
sagar <sagarghimire420@gmail.com>
2021-02-11 14:13:49 -0600

    Initial Commit for Project

---
+++ src/main/java/com/webapp/TextBook/TextBookApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.TextBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webapp/TextBook/TextBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.TextBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
