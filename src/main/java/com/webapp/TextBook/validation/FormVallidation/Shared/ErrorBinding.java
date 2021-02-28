17247993a0f7e8f6159801dc356dbdcadf4a9b2d
Infraction71 <41410700+Infraction71@users.noreply.github.com>
2021-02-27 19:23:44 -0600

    Error Binding complete

--- src/main/java/com/webapp/TextBook/validation/FormVallidation/Shared/ErrorBinding.java
+++ src/main/java/com/webapp/TextBook/validation/FormVallidation/Shared/ErrorBinding.java
@@ -1,31 +1,49 @@
 package com.webapp.TextBook.validation.FormVallidation.Shared;
 import org.javatuples.Pair;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.lang.Nullable;
 
 import java.util.Optional;
-import java.util.Comparator;
-import java.util.ArrayList;
 
 public final class ErrorBinding <T> {
-    private  String fieldErrorName, fieldErrorMessage;
-    private T errorData;
+    private  String _fieldErrorName, _fieldErrorMessage;
+    private T _errorData;
     private final static String FIELD_ERROR_NAME_KEY = "fieldName";
     private final static String FIELD_ERROR_MESSAGE_KEY = "message";
-    private final  String ERROR_DATA_KEY ="faultyData";
-    public  ErrorBinding(String fieldErrorMessage, String fieldErrorName,T errorData){
-        this.fieldErrorMessage = fieldErrorMessage;
-        this.fieldErrorName = fieldErrorName;
-        this.errorData = errorData;
+    private final static String ERROR_DATA_KEY ="faultyData";
+
+    public  ErrorBinding(String fieldErrorMessage, String fieldErrorName, @Nullable T errorData){
+        this._fieldErrorMessage = fieldErrorMessage;
+        this._fieldErrorName = fieldErrorName;
+        this._errorData = errorData;
     }
+
     public Pair<Optional<JSONObject>, Boolean> toJsonString(){
-        Optional<JSONObject>jsn = new Optional<JSONObject>();
+
         //if name or error message is empty return empty and false
-        if(fieldErrorName == null || fieldErrorMessage == null){
-            Pair<Optional<JSONObject>, Boolean> rtrn = new Pair<Optional<JSONObject>, Boolean>(jsn, false);
+        if(_fieldErrorName == null || _fieldErrorMessage == null){
+            return new Pair<Optional<JSONObject>, Boolean>(Optional.empty(), false);
 
         }
+        JSONObject errorBindingJsonObject = new JSONObject();
+        try {
+            errorBindingJsonObject.put(FIELD_ERROR_NAME_KEY, _fieldErrorName);
+            errorBindingJsonObject.put(FIELD_ERROR_MESSAGE_KEY, _fieldErrorMessage);
+            errorBindingJsonObject.put(ERROR_DATA_KEY, _errorData);
+            return new Pair<Optional<JSONObject>, Boolean>(Optional.ofNullable(errorBindingJsonObject), true);
+
+
+        }
+        catch (Exception e){
+            return new Pair<Optional<JSONObject>, Boolean>(Optional.empty(), false);
+        }
+
+
 
     }
+
+
+
 
 
 
