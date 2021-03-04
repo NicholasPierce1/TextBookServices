9edfa1ed1d4b15fed030320048074d807448066f
Zachariah Durbin <42813945+ZachariahDurbin@users.noreply.github.com>
2021-03-04 12:06:14 -0600

    Implementing Database

--- src/main/java/com/webapp/TextBook/TextBookApplication.java
+++ src/main/java/com/webapp/TextBook/TextBookApplication.java
@@ -2,12 +2,24 @@ package com.webapp.TextBook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class TextBookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TextBookApplication.class, args);
+	}
+
+	@Bean
+	public DataSource getDataSource(){
+		return DataSourceBuilder.create()
+				.url("jdbc:oracle:thin@localhost:1521/orcl")
+				.username("textbookservices")
+				.password("Textbookpassword2021")
+				.build();
 	}
 
 }
