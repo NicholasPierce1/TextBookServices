8bb97eb187483ff64a8146c6a9dea83a8afeb0f6
Zachariah Durbin <42813945+ZachariahDurbin@users.noreply.github.com>
2021-03-11 10:58:52 -0600

    basic input

--- src/main/java/com/webapp/TextBook/TextBookApplication.java
+++ src/main/java/com/webapp/TextBook/TextBookApplication.java
@@ -2,13 +2,6 @@ package com.webapp.TextBook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
-import javax.sql.DataSource;
 
 @SpringBootApplication
 public class TextBookApplication {
@@ -16,27 +9,4 @@ public class TextBookApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TextBookApplication.class, args);
 	}
-/*
-	@Bean
-	public DataSource getDataSource() {
-		return DataSourceBuilder.create()
-				.url("jdbcoracle:thin:localhost:1521/orcl")
-				.username("textbookservices")
-				.password("Textbookpassword2021")
-				.build();
-	}
-
- */
-
-	@Bean
-	public DataSource getDataSource(){
-		return DataSourceBuilder.create()
-				.url("jdbc:oracle:thin@localhost:1521/orcl")
-				.username("textbookservices")
-				.password("Textbookpassword2021")
-				.build();
-	}
-
-
-
 }
