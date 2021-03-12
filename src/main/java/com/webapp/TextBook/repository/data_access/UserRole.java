f1ab5e87a8cfab620058a34fe97f2da4ce853b1c
Pierce <S529822@nwmissouri.edu>
2021-03-11 21:31:10 -0600

    appended documentation pertaining to UserRole to explicate the structure and functionality.

--- src/main/java/com/webapp/TextBook/repository/data_access/UserRole.java
+++ src/main/java/com/webapp/TextBook/repository/data_access/UserRole.java
@@ -17,13 +17,17 @@ public enum UserRole {
     Supervisor("Supervisor"), StudentEmployee("StudentEmployee");
 
     /**
+     * <p>
      * Denotes the string literal representation of a nominal value for an UserRole.
      * Usage: for string creation of UserRole for ORM conversion of Oracle's DBO into DA POJO.
+     * </p>
      */
     private final String _NOMINAL_VALUE;
 
     /**
-     * Creates a UserRole
+     * <p>
+     *  Creates a UserRole -- hardcoded, literal values that are set in enum value definition.
+     *</p>
      *
      * @param nominalValue: literal representation of a nominal value for an UserRole
      */
@@ -32,6 +36,10 @@ public enum UserRole {
     }
 
     /**
+     *<p>
+     *     Returns the nominal value (String literal representation)
+     *     of the enum value. Used to facilitate generation of UserRoles.
+     *</p>
      *
      * @return String representing the nominal literal of the UserRole
      */
@@ -40,9 +48,10 @@ public enum UserRole {
     }
 
     /**
-     *
+     *<p>
      * Creates a UserRole from the literal representation of a nominal value for a UserRole.
      * If the nominal string does not match the inputted parameter then an empty optional is given.
+     *</p>
      *
      * @param nominalValue: the string literal value to create an UserRole
      * @return Optional(type parameter one = UserRole) holding the UserRole.
@@ -50,12 +59,16 @@ public enum UserRole {
      */
     public static @NotNull Optional<UserRole> createUserRoleByNominalValue(
             @NotNull final String nominalValue){
+
+        // checks if the nominal String value is equal to any of the enum values
+        // if match then encapsulates the corresponding UserRole in an optional.
         if(nominalValue.equals(UserRole.Supervisor.get_NOMINAL_VALUE()))
             return Optional.of(UserRole.Supervisor);
 
         else if(nominalValue.equals(UserRole.StudentEmployee.get_NOMINAL_VALUE()))
             return Optional.of(UserRole.StudentEmployee);
 
+        // if no match then an empty optional.
         else
             return Optional.empty();
 
