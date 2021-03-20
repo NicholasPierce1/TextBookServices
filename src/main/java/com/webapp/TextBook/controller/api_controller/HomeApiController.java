175216884afec92344b5c7baf71cf787bb07ee6b
Seth <54418905+Sbennett99@users.noreply.github.com>
2021-03-20 15:41:30 -0500

    Controller: API Controller - Started minor work on Checked out books

--- src/main/java/com/webapp/TextBook/controller/api_controller/HomeApiController.java
+++ src/main/java/com/webapp/TextBook/controller/api_controller/HomeApiController.java
@@ -1,5 +1,11 @@
 package com.webapp.TextBook.controller.api_controller;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.tomcat.util.json.JSONParser;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,14 +19,28 @@ public class HomeApiController {
     @RequestMapping(value = "/getCheckedOutBooks", method = RequestMethod.GET,
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public String checkedoutBooks(@RequestBody String jsonString){
+    public String checkedoutBooks(@RequestBody String jsonString) throws JSONException {
 
         try{
+            JSONObject temp = new JSONObject(jsonString);
+            JSONObject loginUser = new JSONObject(temp.getString("loginUserInfo"));
+            JSONObject studentInfo = new JSONObject(temp.getString("studentInfo"));
 
 
 
             return "";
 
+        }catch(JSONException e){
+            JSONObject outputData = new JSONObject();
+            outputData.put("books", null);
+            outputData.put("student", null);
+            outputData.put("bag", null);
+            outputData.put("term", null);
+            outputData.put("statusMessage", null);
+            outputData.put("GeneralError", generalJsonStringErrorMessage);
+            outputData.put("Errors", null);
+
+            return  "";
         }catch(Exception e){
 
             return "";
