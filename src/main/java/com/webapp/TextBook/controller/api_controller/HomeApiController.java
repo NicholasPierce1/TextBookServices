69e98568de3b90d747639e305ecbdf0f959c51a2
Seth <54418905+Sbennett99@users.noreply.github.com>
2021-03-20 19:34:46 -0500

    Controller: API Controller:CheckedOutBooks finished -- Still needs comments

--- src/main/java/com/webapp/TextBook/controller/api_controller/HomeApiController.java
+++ src/main/java/com/webapp/TextBook/controller/api_controller/HomeApiController.java
@@ -5,13 +5,15 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.webapp.TextBook.repository.Bag.BagRepositoryImpl;
 import com.webapp.TextBook.repository.adapter.Adapter;
-import com.webapp.TextBook.repository.data_access.User;
+import com.webapp.TextBook.repository.data_access.*;
+import com.webapp.TextBook.sharedFiles.StatusCode;
 import com.webapp.TextBook.sharedFiles.VerifySessionUser;
 import com.webapp.TextBook.validation.ApiValidation.ApiValidationHandler.ApiValidationHandler;
 import com.webapp.TextBook.validation.Shared.SharedValidationState;
 import com.webapp.TextBook.viewModel.apiViewModel.StudentInfo;
 import com.webapp.TextBook.viewModel.sharedViewModel.loginUserInfo.LoginUserInfo;
 import org.apache.tomcat.util.json.JSONParser;
+import org.javatuples.Quintet;
 import org.javatuples.Triplet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONException;
@@ -25,14 +27,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
 @RequestMapping(path = "/rest/api/inventory")
 public class HomeApiController {
     final static String generalJsonStringErrorMessage = "(HomeAPIController) - Invalid JsonString passed into controller";
+
     @Autowired
     private ApiValidationHandler apiValidationHandler;
+
+    @Autowired
+    Adapter adapter;
 
     @RequestMapping(value = "/getCheckedOutBooks", method = RequestMethod.GET,
             consumes = MediaType.APPLICATION_JSON_VALUE,
@@ -41,7 +48,10 @@ public class HomeApiController {
 
         HttpHeaders headers = new HttpHeaders();
         JSONObject outputData = new JSONObject();
-        Adapter adapter;
+        outputData.put("books", null);
+        outputData.put("student", null);
+        outputData.put("bag", null);
+        outputData.put("term", null);
 
         try{
             JSONObject temp = new JSONObject(jsonString);
@@ -53,10 +63,7 @@ public class HomeApiController {
 
             if(loginUserInfoOptional.isEmpty() || studentInfoOptional.isEmpty()){
 
-                outputData.put("books", null);
-                outputData.put("student", null);
-                outputData.put("bag", null);
-                outputData.put("term", null);
+
                 outputData.put("statusMessage", null);
                 outputData.put("GeneralError", generalJsonStringErrorMessage);
                 outputData.put("Errors", null);
@@ -74,10 +81,6 @@ public class HomeApiController {
             if(loginUserErrorPresent){
                 String loginUserErrorMessage = loginUserInfoValidationBindingErrorMessage.get();
                 final boolean IS_GENERIC = SharedValidationState.isGenericErrorMessage(loginUserErrorMessage);
-                outputData.put("books", null);
-                outputData.put("student", null);
-                outputData.put("bag", null);
-                outputData.put("term", null);
                 outputData.put("statusMessage", null);
                 outputData.put("GeneralError", IS_GENERIC ?  loginUserErrorMessage : null);
                 outputData.put("Errors", !IS_GENERIC ?  loginUserErrorMessage : null);
@@ -85,40 +88,61 @@ public class HomeApiController {
             }else if(studentInfoErrorPresent){
                 String studentInfoErrorMessage = studentInfoValidationBindingErrorMessage.get();
                 final boolean IS_GENERIC = SharedValidationState.isGenericErrorMessage(studentInfoErrorMessage);
-                outputData.put("books", null);
-                outputData.put("student", null);
-                outputData.put("bag", null);
-                outputData.put("term", null);
+
                 outputData.put("statusMessage", null);
                 outputData.put("GeneralError", IS_GENERIC ?  studentInfoErrorMessage : null);
                 outputData.put("Errors", !IS_GENERIC ?  studentInfoErrorMessage : null);
 
             }
             else{
+
                 LoginUserInfo loginUserInfo = loginUserInfoOptional.get();
                 StudentInfo studentInfo = studentInfoOptional.get();
-                Triplet<Boolean, String, Optional<User>> userValidation = VerifySessionUser.isSessionUserValid(loginUserInfo);
+                Triplet<Boolean, String, Optional<User>> userValidation =
+                        VerifySessionUser.isSessionUserValid(loginUserInfo);
                 outputData.put("GeneralError",null);
                 outputData.put("Errors",null);
 
                 if(userValidation.getValue0()){
 
+                    outputData.remove("books");
+                    outputData.remove("student");
+                    outputData.remove("bag");
+                    outputData.remove("term");
+
+                    Quintet<Optional<List<BookCopy>>, Optional<Student>, Optional<Bag>, Optional<Term>, StatusCode>
+                        allCheckedOutBooks = adapter.getAllCheckedOutBooks(
+                                userValidation.getValue2().get(),
+                            studentInfo.getTermCode(),
+                            studentInfo.getId());
 
 
-                    outputData.put("books", null);
-                    outputData.put("student",null);
-                    outputData.put("bag", null);
-                    outputData.put("term", null);
-                    outputData.put("statusMessage", userValidation.getValue1());
-                    return new ResponseEntity<String>(outputData.toString(), headers,
-                            HttpStatus.OK);
+                    if(allCheckedOutBooks.getValue4().equals(StatusCode.OK)) {
+
+                        outputData.put("books", allCheckedOutBooks.getValue0().get());
+                        outputData.put("student", allCheckedOutBooks.getValue1().get());
+                        outputData.put("bag", allCheckedOutBooks.getValue2().get());
+                        outputData.put("term", allCheckedOutBooks.getValue3().get());
+                        outputData.put("statusMessage", allCheckedOutBooks.getValue4().getContentMessage());
+                        return new ResponseEntity<String>(outputData.toString(), headers,
+                                HttpStatus.OK);
+                    }else{
+
+                        outputData.put("books", null);
+                        outputData.put("student", null);
+                        outputData.put("bag", null);
+                        outputData.put("term", null);
+                        outputData.put("statusMessage", allCheckedOutBooks.getValue4().getContentMessage());
+                        return new ResponseEntity<String>(outputData.toString(), headers,
+                                HttpStatus.BAD_REQUEST);
+
+
+                    }
+
 
                 }else{
 
-                    outputData.put("books", null);
-                    outputData.put("student", null);
-                    outputData.put("bag", null);
-                    outputData.put("term", null);
+
                     outputData.put("statusMessage", userValidation.getValue1());
 
                     // Might not be necessary if we want this to be bad_request status
@@ -137,10 +161,7 @@ public class HomeApiController {
 
         }catch(JSONException e){
 
-            outputData.put("books", null);
-            outputData.put("student", null);
-            outputData.put("bag", null);
-            outputData.put("term", null);
+
             outputData.put("statusMessage", null);
             outputData.put("GeneralError", generalJsonStringErrorMessage);
             outputData.put("Errors", null);
@@ -149,6 +170,9 @@ public class HomeApiController {
                     HttpStatus.BAD_REQUEST);
         }catch(Exception e){
 
+            outputData.put("statusMessage", null);
+            outputData.put("GeneralError", "Internal Error: (HomeApiController)");
+            outputData.put("Errors", null);
             return new ResponseEntity<String>(outputData.toString(), headers,
                     HttpStatus.BAD_REQUEST);
         }
