9608b476d72e375107ec92a6c6e936e1517e9c01
Austin Barnhart <42754268+s529215@users.noreply.github.com>
2021-03-21 18:29:21 -0500

    Adding Documentation to data access file Bag

--- src/main/java/com/webapp/TextBook/repository/data_access/Bag.java
+++ src/main/java/com/webapp/TextBook/repository/data_access/Bag.java
@@ -10,48 +10,139 @@ import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.UUID;
 
+/**
+ * <h1>Bag</h1>
+ * <h2>Type: Class</h2>
+ *
+ * Holds information relating to a bag.
+ * These values are determined and fully defined from the business context and are explicated within
+ * the FRD and traceability document
+ */
+
 @Entity
 public class Bag implements DataAccessConversion {
-
+    /**
+     * <p>
+     * GeneratedValue creates a unique id for each bag created
+     * Usage: Hold primary key value for a unique bag instance
+     * </p>
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID _ID;
 
+    /**
+     * <p>
+     * unique string-code that's tailored to a student's 919
+     * Usage: Attribute in creation of Bag for ORM conversion of Oracle's DBO into DA POJO.
+     * </p>
+     */
     public String pidm;
+
+    /**
+     * <p>
+     * the number associated to the student for the current
+     * trimester that they are enrolled & verified for.
+     * Usage: Attribute in creation of Bag for ORM conversion of Oracle's DBO into DA POJO.
+     * </p>
+     */
     public Double bagNumber;
 
+    /**
+     * <p>
+     *  Creates a Bag -- Empty constructor.
+     *</p>
+     */
     public Bag(){this._ID = UUID.randomUUID();}
 
+    /**
+     * <p>
+     *  Creates a Bag -- Full constructor with all variables.
+     *</p>
+     *
+     * @param pidm: String pidm variable
+     * @param bagNumber: char bagNumber variable
+     */
     public Bag(String pidm, Double bagNumber){
         this._ID = this._ID = UUID.randomUUID();
         this.pidm = pidm;
         this.bagNumber = bagNumber;
     }
 
+
+    /**
+     *<p>
+     *     Sets the _ID to a new UUID.
+     *</p>
+     *
+     * @param _ID: UUID _ID variable
+     */
     public void set_ID(UUID _ID) {
         this._ID = _ID;
     }
 
+    /**
+     *<p>
+     *     Returns the _ID.
+     *</p>
+     *
+     * @return UUID _ID information.
+     */
     public UUID get_ID() {
         return _ID;
     }
 
+    /**
+     *<p>
+     *     Sets the pidm to a new string.
+     *</p>
+     *
+     * @param pidm: String pidm variable
+     */
     public void setPidm(String pidm) {
         this.pidm = pidm;
     }
 
+    /**
+     *<p>
+     *     Returns the pidm.
+     *</p>
+     *
+     * @return String pidm information.
+     */
     public String getPidm() {
         return pidm;
     }
 
+    /**
+     *<p>
+     *     Returns the bagNumber.
+     *</p>
+     *
+     * @return Double bagNumber information.
+     */
     public Double getBagNumber() {
         return bagNumber;
     }
 
+    /**
+     *<p>
+     *     Sets the bagNumber to a new Double.
+     *</p>
+     *
+     * @param bagNumber: Double bagNumber variable
+     */
     public void setBagNumber(Double bagNumber) {
         this.bagNumber = bagNumber;
     }
 
+    /**
+     *<p>
+     * Updates a Java Bag object with the values given from the DBO object information.
+     *</p>
+     *
+     * @param values: Generic object holding the DBO information of a Bag
+     */
     @Override
     public void updateDataAccessObject(@NotNull Object[] values) {
 
