24cfc1ca4dc3478c26243cfc7e57fc2cd0fe61a4
Dustin Potter <42753502+dpott60@users.noreply.github.com>
2021-03-22 17:54:24 -0500

    Added documentation for BookCopyRepository

--- src/main/java/com/webapp/TextBook/repository/BookCopy/BookCopyRepository.java
+++ src/main/java/com/webapp/TextBook/repository/BookCopy/BookCopyRepository.java
@@ -6,6 +6,14 @@ import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+/**
+ * <h1>BookCopyRepository</h1>
+ * <h2>Type: Interface</h2>
+ *
+ * Interface to interact with the database exclusively for the BookCopy entity.
+ * It also extends JPA for ORM operations and the custom procedures to interact
+ * with the BookCopy database table.
+ */
 @Repository
 public interface BookCopyRepository extends JpaRepository<BookCopy, UUID>, BookCopyRepositoryCustom{
 }
