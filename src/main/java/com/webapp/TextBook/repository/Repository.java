f6774f2b33594b1e96b3850cf7bed58ba41005c1
Dustin Potter <42753502+dpott60@users.noreply.github.com>
2021-03-21 22:18:05 -0500

    Added documentation for Repository

--- src/main/java/com/webapp/TextBook/repository/Repository.java
+++ src/main/java/com/webapp/TextBook/repository/Repository.java
@@ -2,6 +2,20 @@ package com.webapp.TextBook.repository;
 
 import javax.validation.constraints.NotNull;
 
+/**
+ * <h1>Repository</h1>
+ * <h2>Type: Interface</h2>
+ *
+ * Interface to create stub method to use for all data access repositories
+ */
 public interface Repository {
+
+    /**
+     * <p>
+     *  Stub for GetTableName method to get the necessary database table name needed.
+     *</p>
+     *
+     * @return String, which will be the database table name in query format for the specific table needed.
+     */
     @NotNull String GetTableName();
 }
