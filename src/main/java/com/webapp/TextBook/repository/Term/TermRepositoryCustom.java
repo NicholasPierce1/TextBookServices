ff0167464869c8b2f1161efe8774bf26e86802cd
Austin Barnhart <42754268+s529215@users.noreply.github.com>
2021-03-21 21:36:48 -0500

    Added Documentation to TermRepositoryCustom

--- src/main/java/com/webapp/TextBook/repository/Term/TermRepositoryCustom.java
+++ src/main/java/com/webapp/TextBook/repository/Term/TermRepositoryCustom.java
@@ -7,8 +7,23 @@ import com.webapp.TextBook.sharedFiles.StatusCode;
 import javax.validation.constraints.NotNull;
 import java.util.Optional;
 
+/**
+ * <h1>TermRepositoryCustom</h1>
+ * <h2>Type: Interface</h2>
+ *
+ * Interface to create stubs for custom procedure for Term entity and extends generic
+ * repository that holds the getTableName method to use for all entities.
+ */
 @org.springframework.stereotype.Repository
 public interface TermRepositoryCustom extends Repository {
 
+    /**
+     * <p>
+     *  Stub for getTermWithTermCode procedure for Term database table.
+     *</p>
+     *
+     * @param termCode: String termCode representing the number that denotes the year and trimester.
+     * @return Optional Term object with Status Code
+     */
     @NotNull Pair<Optional<Term>, StatusCode> getTermWithTermCode(@NotNull final String termCode);
 }
