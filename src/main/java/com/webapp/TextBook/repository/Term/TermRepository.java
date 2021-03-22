0729791c604e2cb3dc9eb9d11e6789b448823cc3
Austin Barnhart <42754268+s529215@users.noreply.github.com>
2021-03-21 21:32:51 -0500

    Added Documentation to TermRepository

--- src/main/java/com/webapp/TextBook/repository/Term/TermRepository.java
+++ src/main/java/com/webapp/TextBook/repository/Term/TermRepository.java
@@ -8,6 +8,14 @@ import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+/**
+ * <h1>TermRepository</h1>
+ * <h2>Type: Interface</h2>
+ *
+ * Interface to interact with the database exclusively for the Term entity.
+ * It also extends JPA for ORM operations and the custom procedures to interact
+ * with the Term database table.
+ */
 @Repository
 public interface TermRepository extends JpaRepository<Term, UUID>, TermRepositoryCustom {
 }
