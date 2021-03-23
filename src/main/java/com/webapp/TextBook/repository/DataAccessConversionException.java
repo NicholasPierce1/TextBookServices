e7b551501604625c78b298966be34ecb235ad4b4
Dustin Potter <42753502+dpott60@users.noreply.github.com>
2021-03-22 19:03:46 -0500

    Added documentation for DataAccessConversionException

--- src/main/java/com/webapp/TextBook/repository/DataAccessConversionException.java
+++ src/main/java/com/webapp/TextBook/repository/DataAccessConversionException.java
@@ -1,6 +1,20 @@
 package com.webapp.TextBook.repository;
 
+/**
+ * <h1>DataAccessConversionException</h1>
+ * <h2>Type: Exception Class</h2>
+ *
+ * Exception Class made to add a custom exception for error calls.
+ */
 public class DataAccessConversionException extends RuntimeException{
+
+    /**
+     * <p>
+     *  Custom error throwing method that uses the RuntimeException error call as a source.
+     *</p>
+     *
+     * @param message: String message representing the error message we want to call.
+     */
     public DataAccessConversionException(String message){
         super(message);
     }
