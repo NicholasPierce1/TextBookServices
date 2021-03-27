9e5c17cf5fbcfda4a0a804ef16edc216616cb5d1
Infraction71 <41410700+Infraction71@users.noreply.github.com>
2021-03-27 16:14:11 -0500

    Fixed regex again.

--- src/main/java/com/webapp/TextBook/validation/Shared/RegexPatternContainer.java
+++ src/main/java/com/webapp/TextBook/validation/Shared/RegexPatternContainer.java
@@ -10,6 +10,5 @@ public class RegexPatternContainer {
     public static final Pattern S_NUMBER_PREFIX = Pattern.compile("^[s,S][0-9]{6}(@(.*)|)$");
     public static final Pattern S_NUMBER_SUFFIX= Pattern.compile("^.{7}@.*$");
     public static final Pattern TERM_PATTERN= Pattern.compile("^(19[8-9][0-9]|2[0-9]{3})(10|20|30)$");
-    public static final Pattern BARCODE_PATTERN= Pattern.compile("^317510[0,1][0-9]{5}[" +
-            " Q,S,T,U,V,W,X,Y,Z,-,.,?,+.$,%,$,//, ] $");
+    public static final Pattern BARCODE_PATTERN= Pattern.compile("^317510[0-9]{6}.$");
 }
