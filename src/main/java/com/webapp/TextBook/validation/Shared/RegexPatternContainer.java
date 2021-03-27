ea6339254c59136ec6aa7dcb2680ea52c39b3711
Infraction71 <41410700+Infraction71@users.noreply.github.com>
2021-03-27 16:00:39 -0500

    Another regex fix

--- src/main/java/com/webapp/TextBook/validation/Shared/RegexPatternContainer.java
+++ src/main/java/com/webapp/TextBook/validation/Shared/RegexPatternContainer.java
@@ -9,7 +9,7 @@ public class RegexPatternContainer {
     public static final Pattern STUDENT_ID_PATTERN= Pattern.compile("^919[0-9]{6}$");
     public static final Pattern S_NUMBER_PREFIX = Pattern.compile("^[s,S][0-9]{6}(@(.*)|)$");
     public static final Pattern S_NUMBER_SUFFIX= Pattern.compile("^.{7}@.*$");
-    public static final Pattern TERM_PATTERN= Pattern.compile("^(19[8-9][0-9]|2[0-9]{3})10|20|30$");
+    public static final Pattern TERM_PATTERN= Pattern.compile("^(19[8-9][0-9]|2[0-9]{3})(10|20|30)$");
     public static final Pattern BARCODE_PATTERN= Pattern.compile("^317510[0,1][0-9]{5}[" +
             " Q,S,T,U,V,W,X,Y,Z,-,.,?,+.$,%,$,//, ] $");
 }
