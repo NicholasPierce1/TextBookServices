907e25a49108288a0829808a064a3dd134a9045a
ChaseStaples <s528345@nwmissouri.edu>
2021-04-01 21:09:58 -0500

    Created HomeAjax file
    Added ajax fetchAPI and postData function

---
+++ src/main/resources/META-INF/JS/HomeAjax.js
@@ -0,0 +1,51 @@
+async function fetchAPI(url) {
+    fetch(url)
+        .then(response => {
+            if(response.ok){
+                return response.json();
+            }
+            else{
+                console.log("An Error Occurred")
+                throw Error("Error");
+            }
+        })
+        .then(data => {
+            console.log(data);
+
+        },
+            error => {
+                console.log(`Error in fetch api: ${error}`);
+            })
+        .catch(error =>{
+            console.log(error);
+        })
+
+}
+
+function postData(){
+    fetch("/rest/api/frontEnd", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: ({
+
+        })
+    })
+        .then(response => {
+            if(response.ok){
+                return response.json();
+            }
+            else{
+                console.log("An Error Occurred")
+                throw Error("Error");
+            }
+        },
+            errorReason => {
+            console.log(`An error occured in the fetch api\nreason: ${errorReason}`);
+            })
+        .then(data => {
+            console.log(data)
+        });
+}
