75ad26d1ce1aab2473350024fc0c113c5f7abecc
Pierce <S529822@nwmissouri.edu>
2021-04-19 18:20:03 -0500

    verify session user tests rectified and completed exhaustively

--- src/main/java/com/webapp/TextBook/sharedFiles/StatusCode.java
+++ src/main/java/com/webapp/TextBook/sharedFiles/StatusCode.java
@@ -5,7 +5,7 @@ import javax.validation.constraints.NotNull;
 
 public enum StatusCode {
     BookCopyUndefined(""),
-    DataAccessConversionException(""),
+    DataAccessConversionException("Code: DACE -- " + StatusCode._GENERAL_STATUS_MESSAGE_SUFFIX),
     ArgumentException(""),
     UndefinedPerson(""),
     UndefinedTerm(""),
@@ -25,6 +25,11 @@ public enum StatusCode {
 
     // retains the content message that'll be given to the user as response feedback
     private final String _contentMessage;
+
+    // retains a private static string to be the suffix of status message errors that are general
+    // or abstract to the user
+    private static final String _GENERAL_STATUS_MESSAGE_SUFFIX = "An internal, soft error occurred " +
+            "while processing your request. Please contact IT Support.";
 
     StatusCode(String contentMessage){
         this._contentMessage = contentMessage;
--- src/test/java/com/webapp/TextBook/ValidationTest/Shared/TestValidationBindingHelper.java
+++ src/test/java/com/webapp/TextBook/ValidationTest/Shared/TestValidationBindingHelper.java
@@ -8,6 +8,7 @@ import com.webapp.TextBook.viewModel.apiViewModel.StudentInfo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.Optional;
@@ -15,18 +16,22 @@ import java.util.Optional;
 /**
  * Class tests Validation Binding helper. Looking specifically to get some validation
  */
+@SpringBootTest
 public class TestValidationBindingHelper {
+
     @Autowired
     private ApiValidationHandler validator;
 
     //Creating Valid viewModel objects
-        StudentInfo valid1 = new StudentInfo("s123456", "202020");
-        StudentInfo valid2 = new StudentInfo("s654321", "202010");
-        StudentInfo invalid1 = new StudentInfo("2", "2");
-        StudentInfo invalid2 = new StudentInfo("1", "1");
+    StudentInfo valid1 = new StudentInfo("s123456", "202020");
+    StudentInfo valid2 = new StudentInfo("s654321", "202010");
+    StudentInfo invalid1 = new StudentInfo("2", "2");
+    StudentInfo invalid2 = new StudentInfo("1", "1");
+
+
     @Test
     public void testHandlerApiValidationBindingsForJsonOutput()throws Exception{
-        ArrayList<Optional<String>> listToSend = new ArrayList<>();
+        ArrayList<Optional<String>> listToSend = new ArrayList<Optional<String>>();
         listToSend.add(validator.getApiBindingError(valid1));
         listToSend.add(validator.getApiBindingError(valid2));
         assert (ValidationBindingHelper.handlerApiValidationBindingsForJsonOutput(listToSend, new JSONObject()));
